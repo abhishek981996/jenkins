98581ec1ca00a494a4a11cf71b069e9227cd762d
abhishek981996 <abhishektiwari981996@gmail.com>
2017-05-26 06:29:28 +0000

    changes done

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello yolohealth lets see if it works', done);
+    request(app).get('/').expect('hello yolohealth', done);
   });
 });
