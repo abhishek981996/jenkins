2e2aff01fdbc241c20ae364894fa9da48a823208
abhishek981996 <abhishektiwari981996@gmail.com>
2017-05-26 06:25:25 +0000

    changes done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello yolohealth lets see if it works');
+  res.send('hello yolohealth');
 });
  
 app.listen(process.env.PORT || 5000);
